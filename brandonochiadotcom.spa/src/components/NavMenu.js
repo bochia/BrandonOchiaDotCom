e7f15d35c4f966f5ace2cb4f89d7658a756bd192
Bochia <brandon.ochia@gmail.com>
2023-01-04 00:03:30 +0100

    Removed gap between nav bar and rest of page.

--- brandonochiadotcom.spa/src/components/NavMenu.js
+++ brandonochiadotcom.spa/src/components/NavMenu.js
@@ -24,7 +24,7 @@ export class NavMenu extends Component {
     render() {
         return (
             <header>
-                <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3 navBarBackgroundColor" container light>
+                <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white box-shadow navBarBackgroundColor" container light>
                     <NavbarBrand tag={Link} to="/" className="navBarTextColor">Brandon Ochia</NavbarBrand>
                     <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
                     <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
