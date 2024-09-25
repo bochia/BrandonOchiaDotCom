21b328e5ceb971eb2208742b8aaf29592ef2bff0
Brandon Ochia <brandon.ochia@gmail.com>
2024-09-25 19:30:59 +0200

    Made change to get solution to deploy again.

--- brandonochiadotcom.spa/src/utils/imageUrlBuilder.js
+++ brandonochiadotcom.spa/src/utils/imageUrlBuilder.js
@@ -1,6 +1,5 @@
 function buildImageUrl(imageNameWithExtension) {
     if (!process.env.REACT_APP_BLOB_STORAGE_URL) {
-
         console.log('REACT_APP_BLOB_STORAGE_URL cannot be undefined or empty string.');
     }
 
